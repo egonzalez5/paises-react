9a6c27e6f6c1fd85e2cf1dfe12bb66b62ccb3620
egonzalez5 <eduardo.gonzalez5599@gmail.com>
2021-01-30 20:20:16 -0300

    agrega select con continentes

--- src/App.js
+++ src/App.js
@@ -2,45 +2,64 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 //import Pais from './components/Pais';
 import ListadoPaises from './components/ListadoPaises';
+import Formulario from './components/Formulario';
 
 
 function App() {
 
   const [consultaPais, setConsultaPais] = useState([]);
+  const [busqueda, setBusqueda] = useState({
+    continente:'',
+  });
+  const [consultar, setConsultar] = useState(false)
+
+  
+  const {continente} = busqueda;
 
   useEffect(() => {
 
     const consultarAPI = async () => {
 
-      const url = 'https://restcountries.eu/rest/v2/all';
-      //  https://restcountries.eu/rest/v2/region/{region}
+      if(consultar){
+      //const url = 'https://restcountries.eu/rest/v2/all';
+      const url =  `https://restcountries.eu/rest/v2/region/${continente}`;
       const resultado = await axios.get(url);
       
-
+      //setConsultaPais(consultaPais.data);
       setConsultaPais(resultado.data);
+      setConsultar(false);
       console.log(resultado.data);
+
     }
+  }
     consultarAPI();
+    // eslint-disable-next-line
+  }, [consultar])
 
 
- 
 
   return (
     <div className="container">
       <h1>Página principal</h1>
 
         <div className="row mt-5">
-          <h1>Filtros</h1>
           
-        </div>
-        <div className="col-md-12">
-        <div className="col-md-12">
-          <ListadoPaises
-            consultaPais={consultaPais}
+          <div className="col-md-12">
+          <Formulario
+            busqueda={busqueda}
+            setBusqueda={setBusqueda}
+            setConsultar={setConsultar}
           />
+          </div>
+
+          <div className="col-md-12">
+            <ListadoPaises
+              consultaPais={consultaPais}
+            />
           
-        </div>
+          </div>
       </div>
+
     </div>
   );
 }
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const Formulario = ({busqueda, setBusqueda, setConsultar}) => {
+
+
+
+    // extraer valores
+    const {continente} = busqueda;
+
+    const handleChange = e => {
+        setBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        //enviar informacion a componente principal
+        setConsultar(true);
+    }
+
+
+    return ( 
+        <form
+            onSubmit={handleSubmit}
+        >
+            <div className="form-group">
+                <label htmlFor="continente">Continente</label>
+                <select 
+                    className="form-control" 
+                    name = "continente" 
+                    id = "continente"
+                    value = {continente}
+                    onChange={handleChange}
+                >
+                    <option value="Africa">Africa</option>
+                    <option value="Americas">America</option>
+                    <option value="Asia">Asia</option>
+                    <option value="Europe">Europa</option>
+                    <option value="Oceania">Oceania</option>
+                </select>
+            </div>
+            <div className="">
+                <input
+                    type="submit"
+                    value="Buscar paises"
+                    className="btn btn-primary"
+                />
+            </div>
+        </form>
+
+    );
+}
+ 
+export default Formulario;
